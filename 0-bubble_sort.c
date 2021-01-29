fc324da1b3f5cc4012dfe51f3a4f286eb0a06682
mehdizit <zitounimehdi.it@gmail.com>
2021-01-29 16:28:50 +0100

    up

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,21 +7,20 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j, temp;
-    if (array == NULL || size < 2)
+	unsigned int i, j, temp;
+	if (array == NULL || size < 2)
 		return;
-    for (i = 1; i < size; i++)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-            if(array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
-    
+	for (i = 1; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if(array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
